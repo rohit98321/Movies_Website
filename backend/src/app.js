e89b3876e87842beb17ebd1537c636dd3c9a43a6
rohit gupta <rohitgupta13hai@gmail.com>
2025-09-14 21:04:27 +0530

    checking

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,28 @@
-const express = require("express");
-const movieRoute = require("./routes/movie.router");
-const UserRoute = require("./routes/User.router");
-const cors = require("cors");
+const express =require("express")
+const movieRoute =require("./routes/movie.router")
+const UserRoute=require("./routes/User.router")
+const cors=require("cors")
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser")
 
-const app = express();
 
-app.use(
-  cors({
-    origin: "https://movies-website-1-qu9q.onrender.com",
+const app=express()
+
+app.use(cors({
+    origin:[    
+        "http://localhost:5173",
+        "https://movies-website-1-qu9q.onrender.com"
+    ],
     credentials: true,
-  })
-);
+    exposedHeaders: ["set-cookie"],
+}))
 
-app.use(cookieParser());
-app.use(express.json());
 
-app.use("/movie", movieRoute);
-app.use("/user", UserRoute);
+app.use(cookieParser())
+app.use(express.json())
 
-module.exports = app;
+app.use("/movie",movieRoute)
+app.use("/user",UserRoute)
+
+
+module.exports=app
